c1e996e75f8f9c940664995c28531decf94d7c19
Xzrayz <Donovan.Moua24@student.cart.org>
2025-02-21 08:30:56 -0800

    commit

--- JavaScript.js
+++ JavaScript.js
@@ -1,10 +1,15 @@
-// Basic Example: Smooth Scrolling
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+// Smooth scroll on navigation clicks
+const links = document.querySelectorAll("nav ul li a");
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
+links.forEach(link => {
+    link.addEventListener("click", (event) => {
+        event.preventDefault();
+        const targetId = link.getAttribute("href").substring(1);
+        const targetElement = document.getElementById(targetId);
+        
+        window.scrollTo({
+            top: targetElement.offsetTop - 50,
+            behavior: "smooth"
         });
     });
 });
